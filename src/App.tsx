913706802e5ed8587ff57556028239736ee1a1d2
baekjisu-dev <jisu.dev6476@gmail.com>
2026-02-16 19:58:42 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button>TEST</Button>
+      <p>기본 글꼴 적용 테스트</p>
+      <p className="font-title text-2xl">My Little Pomodoro</p>
+    </div>
+  );
+}
+
+export default App;
