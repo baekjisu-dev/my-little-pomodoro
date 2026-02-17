fb02a47f6b749317002029670ea00212f8f8ffaf
baekjisu-dev <jisu.dev6476@gmail.com>
2026-02-17 14:17:24 +0900

    feat: global layout 구성 변경

--- src/components/layouts/global-layout.tsx
+++ src/components/layouts/global-layout.tsx
@@ -4,13 +4,13 @@ import { ScrollArea } from "../ui/scroll-area";
 
 const GlobalLayout = () => {
   return (
-    <div className="w-full h-full flex justify-center">
-      <div className="w-full max-w-2xl h-full overflow-hidden">
-        <Header />
-        <ScrollArea className="w-full h-[calc(100%-4rem)]">
+    <div className="w-full h-full overflow-hidden">
+      <Header />
+      <ScrollArea className="w-full h-[calc(100%-4rem)]">
+        <div className="w-full max-w-2xl mx-auto">
           <Outlet />
-        </ScrollArea>
-      </div>
+        </div>
+      </ScrollArea>
     </div>
   );
 };
