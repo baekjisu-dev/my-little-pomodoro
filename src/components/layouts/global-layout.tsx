9567a393c070ec70319a0e69c6e3142cc7cdd51d
baekjisu-dev <jisu.dev6476@gmail.com>
2026-02-18 17:15:01 +0900

    fix: global layout 위치 수정

--- src/components/layouts/global-layout.tsx
+++ src/components/layouts/global-layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet, useLocation, useNavigate } from "react-router";
 import Header from "./header";
-import { ScrollArea } from "../ui/scroll-area";
 import { ButtonGroup } from "../ui/button-group";
 import { Button } from "../ui/button";
 import { useMemo } from "react";
@@ -19,25 +18,23 @@ const GlobalLayout = () => {
   };
 
   return (
-    <div className="w-full h-full overflow-hidden">
+    <div className="w-full h-full overflow-hidden flex flex-col justify-center">
       <Header />
-      <ScrollArea className="w-full h-[calc(100%-4rem)]">
-        <div className="w-full max-w-2xl mx-auto">
-          <ButtonGroup className="w-full flex justify-center mt-4">
-            {NAV_TABS.map((tab) => (
-              <Button
-                key={tab.path}
-                className="w-16"
-                variant={activeTabPath === tab.path ? "default" : "outline"}
-                onClick={() => handleTabClick(tab.path)}
-              >
-                {tab.label}
-              </Button>
-            ))}
-          </ButtonGroup>
-          <Outlet />
-        </div>
-      </ScrollArea>
+      <div className="w-full max-w-2xl mx-auto h-135 overflow-auto">
+        <ButtonGroup className="w-full flex justify-center mt-4">
+          {NAV_TABS.map((tab) => (
+            <Button
+              key={tab.path}
+              className="w-16"
+              variant={activeTabPath === tab.path ? "default" : "outline"}
+              onClick={() => handleTabClick(tab.path)}
+            >
+              {tab.label}
+            </Button>
+          ))}
+        </ButtonGroup>
+        <Outlet />
+      </div>
     </div>
   );
 };
